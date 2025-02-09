fabd3a7e12be6b4e1889f27660fea8c04fd0f080
zenbala <balasundarmurugaa@gmail.com>
2025-02-09 14:24:55 +0530

    first commit

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavLinks from '../Navbar/NavLinks';
 import { HashLink } from 'react-router-hash-link';
-
+import companylogo from '../../images/clients/logoWhite.png'; // Adjust the relative path
 
 const NavBar = () => {
     const [top, setTop] = useState(!window.scrollY);
@@ -21,9 +21,14 @@ const NavBar = () => {
 
     return (
         <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
-            <div className="flex flex-row justify-between items-center py-2">
+            <div className="flex flex-row justify-between items-left py-2">
                 <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
-                    <HashLink smooth to="/#hero"><h1 className="font-extrabold text-4xl text-blue-900">mld</h1></HashLink>
+                <HashLink smooth to="/#hero">
+                <img 
+                src={companylogo} 
+                alt="Company Logo" 
+                className="h-16 w-auto"/>
+        </HashLink>
                     
                 </div>
                 <div className="group flex flex-col items-center">
