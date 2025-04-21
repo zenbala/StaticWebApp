68baa09fe58294d508d65349c8924ca18c0685b6
zenbala <balasundarmurugaa@gmail.com>
2025-04-21 20:11:08 +0530

    Small ui fix

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -10,9 +10,9 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#services">
                 Services
             </HashLink>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/">
+            {/* <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/">
                 Portfolio
-            </HashLink>
+            </HashLink> */}
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact#contact">
                 Contact Us
             </HashLink>
